ff38a5bfeae090980bd35809593edd6f68e520ea
spech13 <es-popov2@yandex.ru>
2023-09-18 08:47:58 +0300

    Black formating

--- scheduling.py
+++ scheduling.py
@@ -3,6 +3,7 @@ import threading
 from datetime import datetime, timedelta
 from time import sleep
 
+
 class Scheduling:
     def __init__(self, tasks=[]):
         self.tasks = tasks
@@ -27,18 +28,23 @@ class Scheduling:
                     sleep((date - now).total_seconds())
                 else:
                     raise ValueError("You late")
-                
+
                 return func(*args, **kwargs)
+
             return wrapp
+
         return wrapper
+
 
 @Scheduling.scheduling_decorator(datetime.now() + timedelta(seconds=5))
 def notification_about_train():
     print("You have a train")
 
+
 @Scheduling.scheduling_decorator(datetime(2023, 9, 17, 17, 46, 0))
 def notification_about_doctor():
     print("You have an appointment with a doctor though 30 minutes")
+
 
 scheduling = Scheduling()
 scheduling.extend_tasks((notification_about_doctor, notification_about_train))
